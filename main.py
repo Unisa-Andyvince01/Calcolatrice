334c7b080df259a4fa7832641dac0d7a20813da2
Rolgio10 <116253755+Rolgio10@users.noreply.github.com>
2022-10-20 09:21:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,41 @@ def divisione(*numbers):
             raise ZeroDivisionError('Errrore: Il dividendo è uguale a 0!')
         divisione *= pow(num, -1)
     return divisione
+
+def main():
+
+    scelta=menu()
+    while(scelta!=0):
+        match scelta:
+            case "1":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", addizione(a,b))
+            case "2":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", sottrazione(a,b))
+            case "3":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", moltiplicazione(a,b))
+            case "4":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", divisione(a,b))
+            case "5":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", potenza(a,b))
+            case "6":
+                a=input("Inserire il primo operando: ")
+                b=input("Inserire il secondo operando: ")
+                print("Risultato: ", logaritmo(a,b))   
+            case "a":
+                print("Cronologia Operazioni")
+            case "b":
+                print("Ans")
+            case "0":
+                print("Spegnimento Calcolatrice")
+
+        scelta = menu()
