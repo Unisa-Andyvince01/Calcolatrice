1bf96117f3704f85bbb457c032aa705a412bd6d6
Rolgio10 <116253755+Rolgio10@users.noreply.github.com>
2022-10-20 09:08:59 +0200

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,18 @@
+def menu():
+
+    print("CALCOLATRICE: OPERAZIONI DISPONIBILI")
+    print("1. Addizione")
+    print("2. Sottrazione")
+    print("3. Moltiplicazione")
+    print("4. Divisione")
+    print("5. Potenza")
+    print("6. Logaritmo")
+    print("-------------------------")
+    print("AZIONI DISPONIBILI")
+    print("a. Cronologia Operazioni")
+    print("b. Ans")
+    print("0. Esci")
+
+    print("-------------------------")
+    scelta  = input("Scegli una operazione: ")
+    return scelta
