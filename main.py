644c37577f8aa78ef676e352c7a8df052cf5489e
Andyvince01 <108798058+Andyvince01@users.noreply.github.com>
2022-10-20 09:03:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 /*OPERAZIONI ARITMETICHE*/
 def addizione(a, b):
     if(not isinstance(a, (int, float)) or not isinstance(b, (int, float))):
-        raise TypeError('Non sono stati inseriti numeri interi!')
+        raise TypeError('Non sono stati inseriti numeri reali!')
     return a + b
 
 def sottrazione(a, b):
     if(not isinstance(a, (int, float)) or not isinstance(b, (int, float))):
-        raise TypeError('Non sono stati inseriti numeri interi!')
+        raise TypeError('Non sono stati inseriti numeri reali!')
     return a - b
 
 def moltiplicazione(a, b):
     if(not isinstance(a, (int, float)) or not isinstance(b, (int, float))):
-        raise TypeError('Non sono stati inseriti numeri interi!')
+        raise TypeError('Non sono stati inseriti numeri reali!')
     return a * b
 
 def divisione(a, b):
     if(not isinstance(a, (int, float)) or not isinstance(b, (int, float))):
-        raise TypeError('Non sono stati inseriti numeri interi!')
+        raise TypeError('Non sono stati inseriti numeri reali!')
     if(b == 0):
         raise ZeroDivisionError('Error: Il secondo termine inserito è uguale a 0')
     return a / b
